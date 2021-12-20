56aaac1ad0c02dbfd293b5a440f25c3ad182139c
Benjamin Owusu <90052579+Storerun@users.noreply.github.com>
2021-12-20 18:41:23 +0000

    Add files via upload

---
+++ count number of occurance of characters.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+x= input("write something: ")
+y= input("enter letter : ")
+a=x.count(y)
+
+print("The letter '{}' appears {} times ".format(y,a))
+
+
+# In[ ]:
+
+
+
+
